09aaf95e9a5bb81e398138d32c8c97af64fd2194
Juan <juan.lozada1905@gmail.com>
2023-02-22 23:24:06 -0300

    App.jsx Rutas actualizadas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,49 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css"
+
+import Context from "./Context";
+
+import LandingPage from "./Views/Landingpage"
+import Login from "./Views/Login"
+import Register from "./Views/Register"
+import TenantHome from "./Views/TenantHome"
+import TenantProfile from "./Views/TenantProfile"
+import UserHome from "./Views/UserHome"
+import UserProfile from "./Views/UserProfile"
+import TenantCanchas from "./Views/Canchas/TenantCanchas"
+import UserCanchas from "./Views/Canchas/UserCanchas"
+import TenantAgenda from "./Views/Agendas/TenantAgenda"
+import UserAgenda from "./Views/Agendas/UserAgenda"
+
+const { Provider } = Context;
 
 function App() {
 
   return (
-    <>  
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PageLayout/>}>
-            <Route index element={<HomeView />} />
-            <Route path="/registrarse" element={<RegistrarseView/>} />
-            <Route path="/login" element={<LoginView/>} />
-          </Route>
-          <Route element={<PageLayoutPrivate/>} >
-            <Route path="/dashboard" element={<UserView/>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="App">  
+      <Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PageLayout/>}>
+              <Route index element={<LandingPage />} />
+              <Route path="/login" element={<Login/>} />
+              <Route path="/registrarse" element={<Register/>} />
+
+            </Route>
+            <Route element={<PageLayoutPrivate/>} >
+              <Route path="/TenantHome" element={<TenantHome/>} />
+              <Route path="/TenantProfile" element={<TenantProfile/>} />
+              <Route path="/UserHome" element={<UserHome/>} />
+              <Route path="/UserProfile" element={<UserProfile/>} />
+              <Route path="/TenantCanchas" element={<TenantCanchas/>} />
+              <Route path="/UserCanchas" element={<UserCanchas/>} />
+              <Route path="/TenantAgenda" element={<TenantAgenda/>} />
+              <Route path="/UserAgenda" element={<UserAgenda/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
   )
 }
 
