ff2772b30fa8605912b3883e50c384c46c59c5d8
Juan <juan.lozada1905@gmail.com>
2023-03-07 21:11:46 -0300

    arreglando errores para hacer deploy

--- src/Views/UserHome.jsx
+++ src/Views/UserHome.jsx
@@ -1,11 +1,11 @@
-import ProvAsidePerfil from "../components/ProvAsidePerfil"
+import ProvAsidePerfil from "../Components/ProvAsidePerfil";
 import FooterComponent from "../Components/Footer";
 import UserDashboard from "../Components/UserDashboard";
 
 export default function UserHome() {
   return (
     <div id="perfil" className="bg-light vh-100">
-      <ProvAsidePerfil />
+      <ProvAsidePerfil/>
       <UserDashboard />
       <FooterComponent/>
     </div>
