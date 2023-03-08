84a9f215fecdf0b32bdcf8bd356bf97eb4bcf5bc
Juan <juan.lozada1905@gmail.com>
2023-03-07 22:20:55 -0300

    arreglando el export del vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'build'
   }
-}
+})
